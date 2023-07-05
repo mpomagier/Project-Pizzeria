0f000f548e04938797e91dd23a02bfa874ec8dae
Mateusz Pomagier <mpomagier@gmail.com>
2023-07-05 10:47:41 +0200

    Add orderForm

--- src/js/script.js
+++ src/js/script.js
@@ -53,6 +53,7 @@
   };
 
   class Product {
+
     constructor(id, data) {
       const thisProduct = this;
 
@@ -62,6 +63,8 @@
       thisProduct.renderInMenu();
       thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
 
       console.log('new Product:', thisProduct);
     }
@@ -115,6 +118,32 @@
         thisProduct.element.classList.toggle('active');
 
       });
+    }
+
+    initOrderForm() {
+      const thisProduct = this;
+      console.log('initOrderForm');
+
+      thisProduct.form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs) {
+        input.addEventListener('change', function() {
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+
+    processOrder() {      
+      const thisProduct = this;
+      console.log('processOrder');
     }
   }
 
