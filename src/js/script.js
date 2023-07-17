c542f3682c287ae8db0b36a84e1027d594a95654
Mateusz Pomagier <mpomagier@gmail.com>
2023-07-17 18:03:54 +0200

    Add API & AJAX configuration

--- src/js/script.js
+++ src/js/script.js
@@ -71,7 +71,11 @@
     cart: {
       defaultDeliveryFee: 20,
     },
-
+    db: {
+      url: '//localhost:3131',
+      products: 'products',
+      orders: 'orders',
+    },
   };
 
   const templates = {
@@ -536,21 +540,37 @@
       const thisApp = this;
 
       for(let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
     initData: function() {
       const thisApp = this;
 
-      thisApp.data = dataSource;
+      thisApp.data = {};
+      const url = settings.db.url + '/' + settings.db.products;
+
+      fetch(url)
+        .then(function(rawResponse) {
+          return rawResponse.json();
+        })
+
+        .then(function(parsedResponse) {
+          console.log('parsedResponse: ', parsedResponse);
+
+          /* save parsedResponse as thisApp.data.products */
+          thisApp.data.products = parsedResponse;
+
+          /* execute initMenu method */
+          thisApp.initMenu();
+
+        });
     },
 
     init: function() {
       const thisApp = this;
 
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
     },
 
